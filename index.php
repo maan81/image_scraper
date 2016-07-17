5e6c8cdd7134c2a1a77de140afe8b217fe906ead
maan81 <pranijman@gmail.com>
2016-07-17 20:51:14 +0545

    remote

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if(!empty($_POST['url'])){
 <!doctype html>
 <html>
 <head>
-    <!-- <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 <body>
 
@@ -100,8 +100,8 @@ if(!empty($_POST['url'])){
 
 
 
-    <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
-    <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 </body>
 </html>
